9a160f0cf0c859a7558d120f5831e0f95f8a9f2a
Manuela Bognar <manuelabcamara@gmail.com>
2020-08-08 21:16:12 -0300

    navbar

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'; 
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function TeacherList() {
     return (
-        <h1>Teachers</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis">
+                form
+            </PageHeader>
+        </div>
     )
 }
 
