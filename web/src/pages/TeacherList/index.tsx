b3812a13e62c7f7e9f3d6862b89cf1904f4f7a72
Manuela Bognar <manuelabcamara@gmail.com>
2020-08-09 01:50:04 -0300

    Teacher item

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'; 
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
 
 import './styles.css';
 
@@ -26,6 +28,19 @@ function TeacherList() {
 
                 </form>
             </PageHeader>
+
+            <main>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </main>
+
         </div>
     )
 }
